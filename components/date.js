57650a0137c2f55296ddb9655b814713d092d8d3
Sho Katsuki <shokatsuki3@gmail.com>
2025-09-21 13:04:34 -0700

    nextjs

---
+++ components/date.js
@@ -0,0 +1,15 @@
+import { parseISO, format } from 'date-fns';
+ 
+export default function Date({ dateString }) {
+  //if (dateString !== undefined && dateString !== null && dateString !== "") {
+    const date = parseISO(dateString);
+    try {
+      return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+    } catch (error) {
+      return"";
+    }
+  //} else {
+    //return "";
+  //}
+  
+}
